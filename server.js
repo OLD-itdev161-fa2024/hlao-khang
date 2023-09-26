b606d248026e6c52c3ebe912e9e9bad33e5ccaaf
Hlao Khang <khangh@gmatc.matc.edu>
2023-09-26 11:41:28 -0500

    update mongoose

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 import express from "express";
+import connectDatabase from "./config/db";
 
-// Inistialize express application
+// Initialize express application
 const app = express();
 
+// connect database
+connectDatabase();
+
+// configure middleware
+app.use(express.json({ extended: false }));
+
 // API endpoints
+/**
+ * @route GET /
+ * @desc Text endpoint
+ */
 app.get("/", (req, res) =>
   res.send("http get request sent to root api endpoint")
 );
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 // connection listener
 app.listen(3000, () => console.log("Express server running on port 3000"));
