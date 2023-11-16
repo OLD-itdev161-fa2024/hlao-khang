04467076be17be286f14f4e66afdd355c77a0941
Hlao Khang <khangh@gmatc.matc.edu>
2023-11-15 21:23:22 -0600

    Completed Activity 7

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,34 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
-function App() {
-  let [state, setState] = useState({});
-  useEffect((_) => {
-    axios
-      .get("http://localhost:5000")
-      .then((response) => {
-        setState({ data: response.data });
-      })
-      .catch((error) => console.log(`Error fetching data: ${error}`));
-  });
+const App = () => {
   return (
-    <>
-      <h1>works</h1>
-      {state.data}
-    </>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Good Things</h1>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/register'>Register</Link>
+          </li>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+        </ul>
+      </header>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </main>
+    </div>
   );
-}
+};
 
 export default App;
