adc8d672ffa884a16279443de3b6885d7de46428
Hlao Khang <khangh@gmatc.matc.edu>
2023-12-04 21:27:03 -0600

    added get endpoint /api/posts

--- server.js
+++ server.js
@@ -191,6 +191,20 @@ app.post(
   }
 );
 
+/**
+ * @route GET api/posts
+ * @desc Get posts
+ */
+app.get("/api/posts", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
+
 // connection listener
 const port = 5000;
 app.listen(port, () => console.log("Express server running on port " + port));
